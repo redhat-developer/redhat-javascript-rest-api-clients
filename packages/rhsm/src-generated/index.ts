ec46652f76ec7cfddd4a059808acb7ed1d0c9513
Denis Golovin <dgolovin@redhat.com>
2024-02-02 12:21:58 -0800

    fix: use the same naming pattern throughout repositorty
    * Full name for a client class
    * Abbreviation for a package name
    
    Signed-off-by: Denis Golovin <dgolovin@redhat.com>

--- packages/rhsm/src-generated/index.ts
+++ packages/rhsm/src-generated/index.ts
@@ -2,7 +2,7 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
-export { RhsmClient } from './RhsmClient';
+export { SubscriptionManagerClient } from './SubscriptionManagerClient';
 
 export { ApiError } from './core/ApiError';
 export { BaseHttpRequest } from './core/BaseHttpRequest';
